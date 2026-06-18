c3f4780c82357470507f395aa3de8ad08f900d6e
sanjay-1009 <gskgm2006@gmail.com>
2026-06-18 14:50:38 +0530

    Modified db file for final deployment

--- config/db.js
+++ config/db.js
@@ -2,10 +2,11 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
 
-    host: "localhost",
-    user: "root",
-    password: "admin",
-    database: "spense_db",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
 
     timezone: "+05:30",
     dateStrings: true
